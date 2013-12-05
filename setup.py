fdbbfd44bbc12c3c385fa85e9b58cd9e1fb494ea
Cadyyan <cadyyan@Avalon>
2013-12-05 07:30:22 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+"""
+Setup script for play-cl using setuptools
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+	name			 = 'Play-CL',
+	version			 = '0.0.0.0',
+	description		 = 'Google Play commandline player.',
+	author			 = 'Cadyyan',
+	license			 = 'MIT',
+	packages		 = find_packages(),
+	install_requires = [
+		'gmusicapi'
+	],
+)
+
